06782f9b7c3c7db24024d639a7db6670b6de4a9c
MR.MA <861548313@qq.com>
2019-05-23 23:14:02 +0800

    work

---
+++ src/com/neuedu/test/WorkOne.java
@@ -0,0 +1,16 @@
+package com.neuedu.test;
+
+public class WorkOne {
+    public static void main(String[] args) {
+
+            int i=852;
+            int m=0;
+            while(i!=0){
+                m=i%10;
+                System.out.print(m);
+                i=i/10;
+            }
+
+    }
+    }
+
