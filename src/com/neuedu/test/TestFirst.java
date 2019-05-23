c2afc4f32f8be227060fbe68c57adb37aae8902b
MR.MA <861548313@qq.com>
2019-05-23 19:15:09 +0800

    work

---
+++ src/com/neuedu/test/TestFirst.java
@@ -0,0 +1,29 @@
+package com.neuedu.test;
+// 包 域名的倒序规范
+public class TestFirst {
+    //驼峰命名法 入口函数
+    public static void main(String[]args){
+        System.out.print("hello world!!");
+        /*
+        * 变量  值可以变化的量  java是一个强类型的语言 在变量定义时 必须指定数据类型
+        * int a=1;
+        * System.out.println(a);
+        * a=2;
+        * system.out.println(a);
+        * java 里数据类型 两种数据类型 1.基本数据类型 2.引用数据类型
+        * 基本数据类型：8种 整数类型 byte short int long  float double boolean char
+        * 范围不同 byte 8-128--127 short 2 16位 -5 15次方 ----2的15次方-1  -65536 -----65535
+        * int   4字节 32位 -2的31次方 -----2的31次方 -1 -2147483648 -----2147483647
+        * long  8字节   64位  -2的63次方 ------2的63次方-1
+        *
+        * 常量
+        * 数据类型
+        * 命名规范
+        * 运算符
+        * 流程控制语句
+        *
+        * 语言的特性
+        * */
+
+    }
+}
